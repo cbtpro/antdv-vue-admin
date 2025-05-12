76152ab34dca116a38e1560342f1691226119565
chenbitao <cbtpro@163.com>
2025-05-12 09:44:08 +0800

    ci: 优化打包策略

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,46 @@
-import { fileURLToPath, URL } from 'node:url';
+import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import vueJsx from '@vitejs/plugin-vue-jsx';
-import vueDevTools from 'vite-plugin-vue-devtools';
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: './',
-  plugins: [
-    vue(),
-    vueJsx(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueJsx(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-});
+  build: {
+    sourcemap: true,
+    chunkSizeWarningLimit: 512,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'ant-design-vue': ['ant-design-vue', '@ant-design/icons-vue'],
+          '@tiptap': [
+            '@tiptap/extension-bullet-list',
+            '@tiptap/extension-code-block-lowlight',
+            '@tiptap/extension-heading',
+            '@tiptap/extension-horizontal-rule',
+            '@tiptap/extension-image',
+            '@tiptap/extension-link',
+            '@tiptap/extension-list-item',
+            '@tiptap/extension-ordered-list',
+            '@tiptap/extension-placeholder',
+            '@tiptap/extension-strike',
+            '@tiptap/extension-text-style',
+            // '@tiptap/pm',
+            '@tiptap/starter-kit',
+            '@tiptap/vue-3',
+          ],
+          'vue-bundle': ['pinia', 'vue', 'vue-i18n', 'vue-router'],
+          vuedraggable: ['vuedraggable'],
+        },
+      },
+    },
+  },
+})
